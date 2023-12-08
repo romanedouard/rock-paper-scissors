abaac9e4d719d9808397c742579d232ea1419b72
Roman Edouard <123408909+romanedouard@users.noreply.github.com>
2023-12-08 18:35:59 -0500

    Create a function that plays a single round

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+
+function startRound(playerSelection, computerSelection) {
+    if (playerSelection) {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
