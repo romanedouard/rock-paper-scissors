c6f26452c785daf40ef4054d8ab71184fec260ed
Roman Edouard <123408909+romanedouard@users.noreply.github.com>
2023-12-08 19:09:00 -0500

    improve the startRound function and add methods to the constants

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ function getComputerChoice() {
 }
 
 function startRound(playerSelection, computerSelection) {
-    if (playerSelection) {
-        return `You win! ${playerSelection} beats ${computerSelection}`
+    if (playerSelection === computerSelection) {
+        return `Tie! ${playerSelection} draws with ${computerSelection}!`
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
 console.log(startRound(playerSelection, computerSelection));
+
